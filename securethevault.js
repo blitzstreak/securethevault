366affea110f58eb56c073af70ee289b59d43597
Andy Yu <andy.chenyuhuang@gmail.com>
2024-04-15 14:29:04 -0400

    secure the vault

---
+++ securethevault.js
@@ -0,0 +1,15 @@
+
+// set variables equal to result of the combination
+
+const firstNum = 5 + 5;
+const secondNum = 44 - 4;
+const thirdNum = 13 * 3;
+
+// setting up string that will have a popup for your vault
+
+alert ("You have received this message because you have been chose to open an important vault.");
+
+// pop up will show the combination result of your variables
+
+alert ("Here is the secret combination:" + " " + firstNum + "-" + secondNum + "-" + thirdNum);
+
